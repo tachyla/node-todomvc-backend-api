a84ec4b245cff8370d8170d207fc12fac701e065
tachyla <tachylad.murray@gmail.com>
2017-04-28 10:22:39 -0700

    successful get of 1 item

--- server.js
+++ server.js
@@ -12,19 +12,18 @@ app.use(function(req, res, next){
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
   res.header('Access-Control-Max-Age', '86400');
-  // res.header('location');
   next();
 });
 
-
-//ENDPOINTS HERE
-//GET REQUEST 
+//GET  
 app.get('/api/items', (req, res) => {
   knex.select('id', 'title', 'completed')
-  .from('items')
-  .then((results) => {
-    res.status(200).json(results);
-  });
+    .from('items')
+    .returning('items')
+    .then((response) => {
+      console.log(response);
+      return res.status(200).json(response);
+    });
 });
 
 //POST 
@@ -43,15 +42,33 @@ app.post('/api/items', (req, res) => {
       //here we cannot use res.send! Use .json() accepting in the response/results from Knex
       //since the results are returned as an array 
       //HOWEVER our test requires an object so we pass in results[0] to get the first object 
-      console.log("These are results:" + results);   
-      res.status(201).location(`${res.root}${results[0].id}`).json(results[0]);
+      console.log("These are results:" + results);
+      return res.status(201).location(`${res.root}${results[0].id}`).json(results[0]);
+
+      //include items table 
+
     });
     // .json({ key: 'value' }) should be taking in an object to satisfy our tests
    
 });
 
+app.get('/api/items/:itemId', (req, res) => {
+  // const id = req.params.itemId;
+  const id = parseInt(req.params.itemId);
+  res.json({id});
+});
+
 //PUT
-// app.put('/api/items/:id')
+// app.put('/api/items/:id', (req, res) => {
+//   const reqProperties = ['id', 'title', 'completed'];
+//   const id = req.params.id;
+//   const title = request.body;
+//   knex('stories')
+//     .from({'title': 'req.body'})
+//     .update()
+//     .then(results)=>console.log(results)
+//     res.status(201).json(results);)
+// });
 
 //DELETE 
 
