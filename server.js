f4e406f2ef11065a4f0c5f244e2114512ef04fb4
tachyla <tachylad.murray@gmail.com>
2017-04-28 14:12:57 -0700

    7 successful tests

--- server.js
+++ server.js
@@ -6,16 +6,20 @@ const { DATABASE, PORT } = require('./config');
 const app = express();
 app.use(bodyParser.json());
 
+
+
+
 // ADD EXPRESS MIDDLEWARE FOR CORS HEADERS HERE
 app.use(function(req, res, next){
-  res.header('Access-Control-Allow-Origin','http://chai-http.test');
+  res.header('Access-Control-Allow-Origin',req.get('origin'));
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
-  res.header('Access-Control-Max-Age', '86400');
+  res.header('Access-Control-Max-Age', 86400);
   next();
 });
 
-//GET  
+
+//GET REQUEST of ALL ITEMS
 app.get('/api/items', (req, res) => {
   knex.select('id', 'title', 'completed')
     .from('items')
@@ -26,51 +30,88 @@ app.get('/api/items', (req, res) => {
     });
 });
 
-//POST 
-app.post('/api/items', (req, res) => {
-  console.log(req.body);
-  //set the title as an object = to the request body
-  const {title} = req.body;
-  if(!title) {
-    //used .send to send the message/string to the browser 
-    return res.status(400).send('Missing Title');
-  }
-  knex('items')
-    .insert({'title': req.body.title})
-    .returning(['id','title','completed'])
-    .then((results) => { 
-      //here we cannot use res.send! Use .json() accepting in the response/results from Knex
-      //since the results are returned as an array 
-      //HOWEVER our test requires an object so we pass in results[0] to get the first object 
-      console.log("These are results:" + results);
-      return res.status(201).location(`${res.root}${results[0].id}`).json(results[0]);
-
-      //include items table 
-
-    });
-    // .json({ key: 'value' }) should be taking in an object to satisfy our tests
-   
-});
-
+//GET REQUEST OF ONE ITEM
 app.get('/api/items/:itemId', (req, res) => {
-  // const id = req.params.itemId;
   const id = parseInt(req.params.itemId);
   res.json({id});
 });
 
+//POST 
+
+app.post('/api/items', (req, res) => {
+  console.log(req.body);
+  // const {title} = req.body;
+  const title = req.body.title;
+  if(!title) {
+    return res.status(400).send('Missing Title');
+  }
+
+  knex('items')
+    // .insert({title: title})
+    .insert({'title': title, 'completed': 'false'})
+    .returning(['id','title','completed'])
+    .then((results) => { 
+      const item = results[0];
+      item.url = `${res.root}${results[0].id}`;
+      console.log("These are results:" + results);
+
+      res.status(201).location(item.url).json(item);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).send('Internal Server Error');
+    });
+});
+
+// Alex post request
+// app.post('/api/items', (req, res) => {
+//   const { title } = req.body;
+//   if (!title) {
+//     return res.status(400).send('Missing Title in request body');
+//   }
+
+//   knex.insert({
+//     title: title
+//   })
+//     .into('items')
+//     .returning(['id', 'title', 'completed'])
+//     .then((results) => {
+//       const item = results[0];
+//       item.url = `${res.root}${results[0].id}`;
+//       res.status(201).location(item.url).json(item);
+//     })
+//     .catch(err => {
+//       console.error(err);
+//       res.status(500).send('Internal server error');
+//     });
+// });
+
+
 //PUT
-// app.put('/api/items/:id', (req, res) => {
+//  app.put('/api/items/:id', (req, res) => {
 //   const reqProperties = ['id', 'title', 'completed'];
 //   const id = req.params.id;
 //   const title = request.body;
-//   knex('stories')
-//     .from({'title': 'req.body'})
-//     .update()
-//     .then(results)=>console.log(results)
-//     res.status(201).json(results);)
+//   knex('title')
+//     .from('items')
+//     .where({'id': 'req.body.id'})
+//     .update('title')
+//     .then((results)) => {
+//       console.log(results)
+//       res.status(201).json(results);
+//     });
 // });
 
-//DELETE 
+//DELETE ONE ITEM
+// app.delete('api/items/:id', (req, res) => {
+//   if(field) {
+//     knex('items')
+//       .where({'id': 'request.params.id'})
+//   }
+
+// });
+
+
 
 let server;
 let knex;
